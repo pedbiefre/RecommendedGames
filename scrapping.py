61c7a37ddb281c2700bb41dab8017278c0a27a88
pedbiefre <pbiedmafresno@gmail.com>
2020-12-27 14:04:59 +0100

    Toda la informacion de instant gaming recogida

--- scrapping.py
+++ scrapping.py
@@ -11,12 +11,32 @@ def extraer_juegos():
     page = urlopen(req)
     s = BeautifulSoup(page,"lxml")
     
-    juegos = s.find("div", class_= "search-wrapper").find("div",class_="search").findAll("div",class_="item")
-    titulos = []
-    for juego in juegos:
-        titulo = juego.find("a", class_="cover").find("img").get("title")
-        titulos.append(titulo)
-    return titulos
+    all_link_juegos = s.find("div", class_= "search-wrapper").find("div",class_="search").findAll("div",class_="item")
+    
+    juegos = []
+    
+    for juego in all_link_juegos:
+        
+        titulo = juego.find("div",class_="name").text    
+
+        cover = juego.find("img",class_="picture")['src']
+        
+        price = juego.find("div",class_="price").text
+        price= price.strip("\n")
+
+        link_juego= juego.find("a",class_="cover")['href']
+        req=Request(link_juego,headers=hdr)
+        page_juego=urlopen(req)
+        s = BeautifulSoup(page_juego,"lxml")
+        
+        tags = s.find("div",class_="tags").find_all("a")
+        tags = [tag.text for tag in tags]
+        
+        release = s.find("div",class_="release").find("span").text
+
+        juegos.append((titulo,cover,tags,price,release))
+        
+    return juegos
 
 if __name__ == "__main__":
     print(extraer_juegos())
